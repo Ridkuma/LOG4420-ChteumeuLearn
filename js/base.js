c713b38e22bc85d3ba5fa73923a56aa7b47e594a
jasenjov <javier.asenjo.villamayor@gmail.com>
2014-10-22 14:11:24 -0400

    Question's domains selected stored.

--- js/base.js
+++ js/base.js
@@ -1,6 +1,38 @@
-		$(document).ready(load);
+		$(document).ready(loadSite);
 		
-		function load() {
+		function loadSite() {
 			$('.header').load("header.html");
-			$('.footer').load("footer.html");
+			$('.footer').load("footer.html"); 
+			$( "#formSelect" ).change(getSelected);
+			$("#questionNumber").change(getQuestionNumber);
+			var preg = "";
+			var preg = getQuestion();
+			$('.questionExam').text(preg);
+		};
+
+
+		function getSelected(){
+			var str = [];
+			$('#formSelect option:selected').each(function(){
+				str.push($(this).text());			
+		})
+			sessionStorage.setItem("options", str);
+		};
+
+		function getQuestionNumber(){
+			sessionStorage.setItem("questionNumber", $(this).val());
+		};
+
+		function getQuestion(){
+			var quest = "";
+			var questions = [];
+			var domains = sessionStorage.getItem("options");
+			for(i=0;i<data.length;i++){
+				if(domains.indexOf(data[i].domain) != -1){
+					questions.push(data[i].question);
+					quest = data[i].question;
+				}
+			}
+			sessionStorage.setItem("questions",questions);
+			return quest;
 		};
