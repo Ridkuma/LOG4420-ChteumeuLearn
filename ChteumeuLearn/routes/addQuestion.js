ef471380dd3ffb93778e394d1816345d0cc596ab
Gwenegan Hudin <gwenegan.hudin@insa-rennes.fr>
2014-11-14 14:59:50 -0500

    AddQuestion structure

---
+++ ChteumeuLearn/routes/addQuestion.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET addQuestion page.
+ */
+
+exports.addQuestion = function(req, res){
+  res.render('addQuestion', { title: 'Ajouter Question - Chteumeulearn'});
+};
+
+exports.addAllQuestions = function(req, res){
+  //res.render('addQuestion', { title: 'Ajouter Question - Chteumeulearn'});
+};
