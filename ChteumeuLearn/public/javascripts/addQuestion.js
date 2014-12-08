ad4fb2624c495e9db307729c768e4f8ce7e150c6
Gweylorth <gwenegan.hudin@insa-rennes.fr>
2014-12-07 23:00:18 -0500

    AddQuestion

--- ChteumeuLearn/public/javascripts/addQuestion.js
+++ ChteumeuLearn/public/javascripts/addQuestion.js
@@ -1,54 +1,59 @@
-window.firstKeyPress = true;
-$('.lastAnswer').keypress(onAnswerFieldKeyPress);
-$('.lastAnswer').blur(onAnswerFieldFocusOut);
-$('#addQuestionButton').click(onAddQuestionButtonClicked);
+chteumeulearn.controller('AddQuestionController',
+    function($scope) {
+        
+        $scope.firstKeyPress = true;
+        $('.lastAnswer').keypress(onAnswerFieldKeyPress);
+        $('.lastAnswer').blur(onAnswerFieldFocusOut);
+        $('#addQuestionButton').click(onAddQuestionButtonClicked);
 
-// Add Question
+        // Add Question
 
-function onAnswerFieldKeyPress() {
-    if (window.firstKeyPress) {
-        window.firstKeyPress = false;
-        addAnswerField(this);
-    }
-}
+        function onAnswerFieldKeyPress() {
+            if ($scope.firstKeyPress) {
+                $scope.firstKeyPress = false;
+                addAnswerField(this);
+            }
+        }
 
-function onAnswerFieldFocusOut() {
-    window.firstKeyPress = true;
-    if (!$(this).val()) {
-        removeAnswerField(this);    
-    }
-}
+        function onAnswerFieldFocusOut() {
+            $scope.firstKeyPress = true;
+            if (!$(this).val()) {
+                removeAnswerField(this);    
+            }
+        }
 
-function onAddQuestionButtonClicked() {
-    $('#correctId').val($('#answers').children('input[name=correct]:checked').index('input[name=correct]'));
-    var answers = $('#answers').children('input[name=answer]:not(.lastAnswer)');
-    var string = "";
-    for (var i = 0; i < answers.length; i++) {
-        string += $(answers[i]).val() + ", ";
-    };
+        function onAddQuestionButtonClicked() {
+            $('#correctId').val($('#answers').children('input[name=correct]:checked').index('input[name=correct]'));
+            var answers = $('#answers').children('input[name=answer]:not(.lastAnswer)');
+            var string = "";
+            for (var i = 0; i < answers.length; i++) {
+                string += $(answers[i]).val() + ", ";
+            };
 
-    string = string.slice(0, string.length - 2);
-    $('#answersList').val(string);
-}
+            string = string.slice(0, string.length - 2);
+            $('#answersList').val(string);
+        }
 
-function addAnswerField(origin) {
-    $(origin).removeClass('lastAnswer');
-    var nextField = $('<input type="text" class="lastAnswer" name="answer"></input>');
-    nextField.val(' ');
-    if ($('input[name=answer]').length < 2) {
-        nextField.attr('required', true);
-    }
-    nextField.keypress(onAnswerFieldKeyPress);
-    nextField.blur(onAnswerFieldFocusOut);
-    $(origin).after(nextField);
-    $(origin).before('<input type="radio" name="correct" required></input>');
-    $(origin).after('<br/>');
-    $(origin).off('keypress');
-}
+        function addAnswerField(origin) {
+            $(origin).removeClass('lastAnswer');
+            var nextField = $('<input type="text" class="lastAnswer" name="answer"></input>');
+            nextField.val(' ');
+            if ($('input[name=answer]').length < 2) {
+                nextField.attr('required', true);
+            }
+            nextField.keypress(onAnswerFieldKeyPress);
+            nextField.blur(onAnswerFieldFocusOut);
+            $(origin).after(nextField);
+            $(origin).before('<input type="radio" name="correct" required></input>');
+            $(origin).after('<br/>');
+            $(origin).off('keypress');
+        }
 
-function removeAnswerField(origin) {
-    origin = $(origin);
-    origin.prev().remove();
-    origin.prev().remove();
-    origin.remove();
-}
+        function removeAnswerField(origin) {
+            origin = $(origin);
+            origin.prev().remove();
+            origin.prev().remove();
+            origin.remove();
+        }
+        
+    });
